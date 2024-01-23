27a46f8bde3937d7201209d1c8832dfb517eff5a
Karol <socer404@gmail.com>
2024-01-23 17:19:30 +0100

    Template for plotly webapp

---
+++ webapp/config.py
@@ -0,0 +1,5 @@
+HOST: str = "localhost"  # REDIS HOST
+PORT: int = 6379  # REDIS PORT
+BASE_URL: str = "http://tesla.iem.pw.edu.pl:9080/v2/monitor/"  # URL to endpoint with data
+IDS = range(1, 7)  # IDs of ppl to request data about
+SENSORS_ID = range(6)
